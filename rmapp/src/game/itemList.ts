5e4169a17766935a967b89d8be805f543d67af8d
Val Allen Samonte <val.samonte@gmail.com>
2025-03-14 07:47:58 +0800

    Character Creation

---
+++ rmapp/src/game/itemList.ts
@@ -0,0 +1,243 @@
+export const itemDetails: any = {
+  '': {
+    name: ' ',
+    desc: 'Please select parts and press NEXT.',
+  },
+  head_0: {
+    name: 'HERMES HEAD',
+    desc: 'Light head. Contains agile CPU.',
+    details: {
+      img: '/head_light.png',
+      stats: [['WGT', 2]],
+      cpu: [
+        'bat.10',
+        'roll 10',
+        'w.slip',
+        'grounded',
+        'no ammo',
+        'w.lean',
+        /*
+        0 is grounded
+        1 wall leaning 
+        2 wall slipping
+        3 is flipped
+        4 bat 10
+        5 bat 20
+        6 bat 50
+        7 no ammo
+        8 roll 20
+        9 roll 10
+        19 always
+
+        0 run
+        1 turn
+        2 jump
+        3 w.jump
+        4 flip gravity
+        5 recharge
+        7 shoot
+        8 reload
+
+        [4, 5, 0],
+        // [9, 4, 20],
+        [2, 3, 6],
+        [8, 2, 3],
+        [7, 8, 0],
+        [8, 7, 0],
+        [1, 1, 1],
+        [10, 0, 0],
+        */
+      ],
+      protection: [['punct', 3]],
+    },
+  },
+  head_2: {
+    name: 'HERCULES HEAD',
+    desc: 'Heavy head with all rounder CPU.',
+    details: {
+      img: '/head_heavy.png',
+      stats: [['WGT', 5]],
+      cpu: ['bat.20', 'roll 20', 'no ammo', 'roll 20', 'w.lean', 'w.slip'],
+      protection: [
+        ['punct', 6],
+        ['blast', 3],
+      ],
+    },
+  },
+  body_0: {
+    name: 'HERMES BODY',
+    desc: 'Light body capable of fast charging.',
+    details: {
+      img: '/body_light.png',
+      stats: [
+        ['POW', 18],
+        ['GEN', '1.1'],
+        ['WGT', 8],
+      ],
+      actions: [['recharge', 0]],
+      protection: [['punct', 10]],
+    },
+  },
+  body_2: {
+    name: 'HERCULES BODY',
+    desc: 'More power output in exchange of slow generator.',
+    details: {
+      img: '/body_heavy.png',
+      stats: [
+        ['POW', 38],
+        ['GEN', '1.2'],
+        ['WGT', 12],
+      ],
+      actions: [['recharge', 0]],
+      protection: [
+        ['punct', 12],
+        ['blast', 8],
+      ],
+    },
+  },
+  legs_0: {
+    name: 'HERMES LEGS',
+    desc: 'Unlocks wall jumping.',
+    details: {
+      img: '/legs_light.png',
+      stats: [['WGT', 4]],
+      actions: [
+        ['w.jump', 6],
+        ['jump', 3],
+        ['turn', 1],
+        ['run', 0],
+      ],
+      protection: [['punct', 3]],
+    },
+  },
+  legs_2: {
+    name: 'HERCULES LEGS',
+    desc: 'Heavy duty legs that offers better protection.',
+    details: {
+      img: '/legs_light.png',
+      stats: [['WGT', 8]],
+      actions: [
+        ['jump', 3],
+        ['turn', 1],
+        ['run', 0],
+      ],
+      protection: [
+        ['punct', 6],
+        ['blast', 3],
+      ],
+    },
+  },
+  hg_0: {
+    name: 'P420',
+    desc: 'All rounder weapon with high ROF.',
+    details: {
+      img: '/hg_0.png',
+      stats: [
+        ['ELEM', 'punct'],
+        ['DMG', 4],
+        ['ROF', 15],
+        ['AMMO', 12],
+        ['RCD', 60],
+        ['WGT', 2],
+      ],
+      actions: [
+        ['shoot', 2],
+        ['reload', 5],
+      ],
+    },
+  },
+  hg_1: {
+    name: 'MONGOOSE',
+    desc: 'Powerful magnum with slow ROF.',
+    details: {
+      img: '/hg_1.png',
+      stats: [
+        ['ELEM', 'punct'],
+        ['DMG', 10],
+        ['ROF', 120],
+        ['AMMO', 6],
+        ['RCD', 90],
+        ['WGT', 4],
+      ],
+      actions: [
+        ['shoot', 2],
+        ['reload', 5],
+      ],
+    },
+  },
+  hg_2: {
+    name: 'Q2.BLSTR',
+    desc: 'Shoots energy projectiles.',
+    details: {
+      img: '/hg_2.png',
+      stats: [
+        ['ELEM', 'heat'],
+        ['DMG', 6],
+        ['ROF', 30],
+        ['AMMO', 3],
+        ['RCD', 60],
+        ['WGT', 3],
+      ],
+      actions: [
+        ['shoot', 2],
+        ['reload', 5],
+      ],
+    },
+  },
+  gl_0: {
+    name: 'HKP',
+    desc: '40mm grenade launcher. Explodes on impact.',
+    details: {
+      img: '/gl_0.png',
+      stats: [
+        ['ELEM', 'blast'],
+        ['DMG', 24],
+        ['AMMO', 1],
+        ['RCD', 240],
+        ['WGT', 8],
+      ],
+      actions: [
+        ['shoot', 2],
+        ['reload', 10],
+      ],
+    },
+  },
+  gl_1: {
+    name: 'Q2.GL',
+    desc: 'High ROF GL. Bounces off walls.',
+    details: {
+      img: '/gl_1.png',
+      stats: [
+        ['ELEM', 'blast'],
+        ['DMG', 16],
+        ['ROF', 60],
+        ['AMMO', 3],
+        ['RCD', 240],
+        ['WGT', 16],
+      ],
+      actions: [
+        ['shoot', 2],
+        ['reload', 10],
+      ],
+    },
+  },
+  gl_2: {
+    name: 'MM1',
+    desc: '40mm GL with large magazine.',
+    details: {
+      img: '/gl_2.png',
+      stats: [
+        ['ELEM', 'blast'],
+        ['DMG', 8],
+        ['ROF', 60],
+        ['AMMO', 24],
+        ['RCD', 240],
+        ['WGT', 24],
+      ],
+      actions: [
+        ['shoot', 2],
+        ['reload', 10],
+      ],
+    },
+  },
+}
--- rmapp/src/game/pages/CharacterCreationScreen.tsx
+++ rmapp/src/game/pages/CharacterCreationScreen.tsx
@@ -1,48 +1,428 @@
-import { useAtomValue } from 'jotai'
-import { viewModeAtom } from '../../atoms/viewModeAtom'
 import cn from 'classnames'
-import { useMeasure } from '@uidotdev/usehooks'
-import { useMemo } from 'react'
 import { CharacterPreview } from '../components/CharacterPreview'
+import { SpriteText } from '../components/SpriteText'
+import { useMemo, useState } from 'react'
+import { itemDetails } from '../itemList'
+
+const weapons = ['hg_0', 'hg_1', 'hg_2', 'gl_0', 'gl_1', 'gl_2']
 
 export function CharacterCreationScreen() {
-  const viewMode = useAtomValue(viewModeAtom)
-  const [containerRef, container] = useMeasure()
+  const [head, setHead] = useState('0')
+  const [body, setBody] = useState('0')
+  const [legs, setLegs] = useState('0')
+  const [weapon, setWeapon] = useState('hg_0')
+  const [lastSelected, setLastSelected] = useState('')
 
-  const sizes = useMemo(() => {
-    if (!container.width) return null
-    // assume view mode is 3 always for now
-    const sides = Math.floor((container.width * 0.3) / 8) * 8
-    const main = Math.floor((container.width * 0.4) / 8) * 8
+  const overallStats = useMemo(() => {
+    let generator =
+      itemDetails['body_' + body].details?.stats?.find(
+        ([key]: any) => key === 'GEN'
+      )[1] ?? '1.1'
+    let power =
+      itemDetails['body_' + body].details?.stats?.find(
+        ([key]: any) => key === 'POW'
+      )[1] ?? 0
+    let weight =
+      (itemDetails['head_' + head].details?.stats?.find(
+        ([key]: any) => key === 'WGT'
+      )[1] ?? 0) +
+      (itemDetails['body_' + body].details?.stats?.find(
+        ([key]: any) => key === 'WGT'
+      )[1] ?? 0) +
+      (itemDetails['legs_' + legs].details?.stats?.find(
+        ([key]: any) => key === 'WGT'
+      )[1] ?? 0) +
+      (itemDetails[weapon].details?.stats?.find(
+        ([key]: any) => key === 'WGT'
+      )[1] ?? 0)
 
-    return { sides, main }
-  }, [container, viewMode])
+    let overweight = power / weight
+
+    const armor: number[] = [
+      0, // punct
+      0, // blast
+    ]
+    itemDetails['head_' + head].details.protection.forEach(
+      ([elem, val]: any) => {
+        if (elem === 'punct') {
+          armor[0] += val
+        } else if (elem === 'blast') {
+          armor[1] += val
+        }
+      }
+    )
+    itemDetails['body_' + body].details.protection.forEach(
+      ([elem, val]: any) => {
+        if (elem === 'punct') {
+          armor[0] += val
+        } else if (elem === 'blast') {
+          armor[1] += val
+        }
+      }
+    )
+    itemDetails['legs_' + legs].details.protection.forEach(
+      ([elem, val]: any) => {
+        if (elem === 'punct') {
+          armor[0] += val
+        } else if (elem === 'blast') {
+          armor[1] += val
+        }
+      }
+    )
+
+    return {
+      stats: [
+        ['HP', 100],
+        ['ENRG', 100],
+        ['POW', power],
+        ['GEN', generator],
+        ['WGT', weight],
+      ],
+      overweight,
+      armor,
+    }
+  }, [head, body, legs, weapon])
 
   return (
-    <div
-      ref={containerRef}
-      className="w-full h-full bg-slate-900 items-center justify-center"
-    >
-      <CharacterPreview head="2" body="2" legs="2" weapon="hg_2" />
-      {/* <div
-        className={cn(
-          'flex justify-between h-full',
-          viewMode === 1 && 'flex-col'
-        )}
-      >
-        <div
-          style={{ width: sizes ? sizes.sides + 'px' : undefined }}
-          className={cn('h-full bg-green-400')}
-        ></div>
-        <div
-          style={{ width: sizes ? sizes.main + 'px' : undefined }}
-          className={cn('h-full bg-blue-400')}
-        ></div>
-        <div
-          style={{ width: sizes ? sizes.sides + 'px' : undefined }}
-          className={cn('h-full bg-green-400')}
-        ></div>
-      </div> */}
+    <div className="w-full h-full items-center justify-center p-[1rem]">
+      <div className="flex flex-col gap-[0.5rem] h-full">
+        <div className="border-white border-[0.25em] bg-slate-800 p-[1rem] flex justify-center">
+          <SpriteText>CREATE ROBOT MASTER</SpriteText>
+        </div>
+        <div className="flex flex-auto gap-[0.5rem]">
+          <div className="border-white border-[0.25em] bg-slate-800 py-[1rem] flex-1">
+            <div className="flex flex-col flex-auto gap-[1rem]">
+              <div className="flex items-center justify-center scale-200 p-[2em]">
+                <CharacterPreview
+                  head={head}
+                  body={body}
+                  legs={legs}
+                  weapon={weapon}
+                />
+              </div>
+
+              <div className="w-full border-slate-700 border-b-[0.125em]" />
+              <div className="flex flex-col px-[1rem] gap-[1rem]">
+                {overallStats.stats.map(([key, val]: any, i: number) => (
+                  <div
+                    key={`${key}_${i}`}
+                    className="flex items-center justify-between"
+                  >
+                    <SpriteText>{key.toUpperCase()}</SpriteText>
+                    <SpriteText>{val + ''}</SpriteText>
+                  </div>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <div className="px-[1rem]">
+                  <SpriteText>ARMOR</SpriteText>
+                </div>
+                <div className="flex flex-wrap px-[1rem] gap-[1rem]">
+                  {overallStats.armor.map((val: number, i: number) => {
+                    if (val === 0) return null
+                    const key = ['punct', 'blast'][i]
+                    return (
+                      <div
+                        key={`armor_${i}`}
+                        className="flex items-center gap-[0.25rem]"
+                      >
+                        <img
+                          src={`/elem_${key}.png`}
+                          alt={`elem_${key}`}
+                          className="w-[1rem] h-[1rem]"
+                        />
+                        <SpriteText>{val}</SpriteText>
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
+              {overallStats.overweight < 1 && (
+                <div className="flex px-[1rem] justify-between opacity-50">
+                  <SpriteText>OVERWEIGHT</SpriteText>
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="border-white border-[0.25em] bg-slate-800 pt-[1rem] flex-2 flex flex-col">
+            <div className="flex flex-col flex-auto gap-[1rem]">
+              <div className="flex items-center justify-between px-[1rem]">
+                <SpriteText>HEAD</SpriteText>
+                <div className="flex gap-[1rem]">
+                  <button
+                    onClick={() => {
+                      setHead('0')
+                      setLastSelected('head_0')
+                    }}
+                    className={cn(
+                      'cursor-pointer flex gap-[0.5rem] items-center',
+                      head === '0' && 'bg-blue-400'
+                    )}
+                  >
+                    <img
+                      src="/head_light.png"
+                      alt="head_light"
+                      className="w-[2rem] h-[2rem]"
+                      draggable={false}
+                    />
+                  </button>
+                  <button
+                    onClick={() => {
+                      setHead('2')
+                      setLastSelected('head_2')
+                    }}
+                    className={cn(
+                      'cursor-pointer flex gap-[0.5rem] items-center',
+                      head === '2' && 'bg-blue-400'
+                    )}
+                  >
+                    <img
+                      src="/head_heavy.png"
+                      alt="head_heavy"
+                      className="w-[2rem] h-[2rem]"
+                      draggable={false}
+                    />
+                  </button>
+                </div>
+              </div>
+              <div className="w-full border-slate-700 border-b-[0.125em]" />
+              <div className="flex items-center justify-between px-[1rem]">
+                <SpriteText>BODY</SpriteText>
+                <div className="flex gap-[1rem]">
+                  <button
+                    onClick={() => {
+                      setBody('0')
+                      setLastSelected('body_0')
+                    }}
+                    className={cn(
+                      'cursor-pointer flex gap-[0.5rem] items-center',
+                      body === '0' && 'bg-blue-400'
+                    )}
+                  >
+                    <img
+                      src="/body_light.png"
+                      alt="body_light"
+                      className="w-[2rem] h-[2rem]"
+                      draggable={false}
+                    />
+                  </button>
+                  <button
+                    onClick={() => {
+                      setBody('2')
+                      setLastSelected('body_2')
+                    }}
+                    className={cn(
+                      'cursor-pointer flex gap-[0.5rem] items-center',
+                      body === '2' && 'bg-blue-400'
+                    )}
+                  >
+                    <img
+                      src="/body_heavy.png"
+                      alt="body_heavy"
+                      className="w-[2rem] h-[2rem]"
+                      draggable={false}
+                    />
+                  </button>
+                </div>
+              </div>
+              <div className="w-full border-slate-700 border-b-[0.125em]" />
+              <div className="flex items-center justify-between px-[1rem]">
+                <SpriteText>LEGS</SpriteText>
+                <div className="flex gap-[1rem]">
+                  <button
+                    onClick={() => {
+                      setLegs('0')
+                      setLastSelected('legs_0')
+                    }}
+                    className={cn(
+                      'cursor-pointer flex gap-[0.5rem] items-center',
+                      legs === '0' && 'bg-blue-400'
+                    )}
+                  >
+                    <img
+                      src="/legs_light.png"
+                      alt="legs_light"
+                      className="w-[2rem] h-[2rem]"
+                      draggable={false}
+                    />
+                  </button>
+                  <button
+                    onClick={() => {
+                      setLegs('2')
+                      setLastSelected('legs_2')
+                    }}
+                    className={cn(
+                      'cursor-pointer flex gap-[0.5rem] items-center',
+                      legs === '2' && 'bg-blue-400'
+                    )}
+                  >
+                    <img
+                      src="/legs_heavy.png"
+                      alt="legs_heavy"
+                      className="w-[2rem] h-[2rem]"
+                      draggable={false}
+                    />
+                  </button>
+                </div>
+              </div>
+              <div className="w-full border-slate-700 border-b-[0.125em]" />
+              <div className="flex flex-col px-[1rem] gap-[1rem]">
+                <SpriteText>WEAPON</SpriteText>
+                <div className="flex flex-wrap gap-[1rem]">
+                  {weapons.map((name) => (
+                    <button
+                      key={name}
+                      onClick={() => {
+                        setWeapon(name)
+                        setLastSelected(name)
+                      }}
+                      className={cn(
+                        'cursor-pointer flex gap-[0.5rem] items-center',
+                        weapon === name && 'bg-blue-400'
+                      )}
+                    >
+                      <img
+                        src={`/${name}.png`}
+                        alt={name}
+                        className="w-[4rem] h-[2rem]"
+                        draggable={false}
+                      />
+                    </button>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="p-[1rem] border-white border-t-[0.125em] flex justify-center">
+              <SpriteText>NEXT</SpriteText>
+            </div>
+          </div>
+          <div className="border-white border-[0.25em] bg-slate-800 py-[1rem] flex-1">
+            {itemDetails[lastSelected]?.details && (
+              <div className="flex flex-col gap-[1rem]">
+                <div className="flex px-[1rem] justify-between items-center">
+                  {itemDetails[lastSelected]?.name && (
+                    <SpriteText>{itemDetails[lastSelected].name}</SpriteText>
+                  )}
+                </div>
+
+                {itemDetails[lastSelected].details?.stats && (
+                  <>
+                    <div className="w-full border-slate-700 border-b-[0.125em]" />
+                    <div className="flex flex-col px-[1rem] gap-[1rem]">
+                      {itemDetails[lastSelected].details.stats.map(
+                        ([key, val]: any) => (
+                          <div
+                            key={key}
+                            className="flex items-center justify-between"
+                          >
+                            <SpriteText>{key}</SpriteText>
+                            <div className="flex gap-[0.25rem]">
+                              {key === 'ELEM' && (
+                                <img
+                                  src={`/elem_${val}.png`}
+                                  alt={`elem_${val}`}
+                                  className="w-[1rem] h-[1rem]"
+                                />
+                              )}
+                              <SpriteText>
+                                {(val + '').toUpperCase()}
+                              </SpriteText>
+                            </div>
+                          </div>
+                        )
+                      )}
+                    </div>
+                  </>
+                )}
+                {itemDetails[lastSelected].details?.protection && (
+                  <>
+                    <div className="w-full border-slate-700 border-b-[0.125em]" />
+                    <div className="flex justify-between items-center">
+                      <div className="opacity-50 px-[1rem]">
+                        <SpriteText>ARMOR</SpriteText>
+                      </div>
+                      <div className="flex flex-wrap px-[1rem] gap-[1rem]">
+                        {itemDetails[lastSelected].details.protection.map(
+                          ([elem, pts]: any, i: number) => (
+                            <div
+                              key={`${elem}_${i}`}
+                              className="flex items-center gap-[0.25rem]"
+                            >
+                              <img
+                                src={`/elem_${elem}.png`}
+                                alt={`elem_${elem}`}
+                                className="w-[1rem] h-[1rem]"
+                              />
+                              <SpriteText>{pts}</SpriteText>
+                            </div>
+                          )
+                        )}
+                      </div>
+                    </div>
+                  </>
+                )}
+                {itemDetails[lastSelected].details?.actions && (
+                  <>
+                    <div className="w-full border-slate-700 border-b-[0.125em]" />
+                    <div className="opacity-50 px-[1rem]">
+                      <SpriteText>ACTIONS</SpriteText>
+                    </div>
+                    <div className="flex flex-col px-[1rem] gap-[1rem]">
+                      {itemDetails[lastSelected].details.actions.map(
+                        ([action, cost]: any) => (
+                          <div
+                            key={action}
+                            className="flex items-center justify-between"
+                          >
+                            <SpriteText>{action.toUpperCase()}</SpriteText>
+                            <div className="flex gap-[0.25rem]">
+                              <img
+                                src={`/energy.png`}
+                                alt={`energy`}
+                                className="w-[1rem] h-[1rem]"
+                              />
+                              <SpriteText>{cost}</SpriteText>
+                            </div>
+                          </div>
+                        )
+                      )}
+                    </div>
+                  </>
+                )}
+                {itemDetails[lastSelected].details?.cpu && (
+                  <>
+                    <div className="w-full border-slate-700 border-b-[0.125em]" />
+                    <div className="opacity-50 px-[1rem]">
+                      <SpriteText>CPU</SpriteText>
+                    </div>
+                    <div className="flex flex-col px-[1rem] gap-[1rem]">
+                      {itemDetails[lastSelected].details.cpu.map(
+                        (cpu: string, i: number) => (
+                          <div
+                            key={`${cpu}_${i}`}
+                            className="flex items-center justify-between"
+                          >
+                            <SpriteText>{i + 1}</SpriteText>
+                            <SpriteText>{cpu.toUpperCase()}</SpriteText>
+                          </div>
+                        )
+                      )}
+                    </div>
+                  </>
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="border-white border-[0.25em] bg-slate-800 p-[1rem] flex flex-col gap-[0.5rem]">
+          {itemDetails[lastSelected]?.name && (
+            <SpriteText>{itemDetails[lastSelected].name}</SpriteText>
+          )}
+          {itemDetails[lastSelected]?.desc && (
+            <SpriteText>{itemDetails[lastSelected].desc}</SpriteText>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
