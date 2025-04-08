a6495fd55024d3b03f5a1bb393240503cde25847
Val Allen Samonte <val.samonte@gmail.com>
2025-04-09 00:48:21 +0800

    Demo initialization

--- anchor/programs/robot-masters/src/lib.rs
+++ anchor/programs/robot-masters/src/lib.rs
@@ -1,168 +1,3 @@
-// use anchor_lang::prelude::*;
-
-// use rmengine::{
-//     components::{
-//         Behavior, Behaviors, CharacterTag, CollisionSides, Direction, Energy, Group, Health,
-//         Invulnerability, Position, PowerOutput, Protection, Size, Velocity, Weapon, Weapons,
-//     },
-//     fix16::Fix16,
-//     game::GameState,
-//     lcg::LCG,
-//     World,
-// };
-
-// declare_id!("YUe8UbyQ88GmfjUpDE6TwoUbqwaj6qteWqd2VLWoVFm");
-
-// #[program]
-// pub mod robot_masters {
-
-//     use super::*;
-
-//     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-//         msg!("Greetings from: {:?}", ctx.program_id);
-
-//         let game_state = GameState {
-//             state: 0,
-//             game_over_since: None,
-//             winner: None,
-//             frame: 0,
-//             gravity: 50,
-//             seed: 12345,
-//             tile_map: vec![
-//                 vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-//                 vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-//                 vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-//                 vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-//                 vec![1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-//                 vec![1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-//                 vec![1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1],
-//                 vec![1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1],
-//                 vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-//                 vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-//                 vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-//                 vec![1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-//                 vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-//                 vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-//                 vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-//             ],
-//             to_spawn_projectiles: Vec::new(),
-//             to_despawn_projectiles: Vec::new(),
-//             to_damage: Vec::new(),
-//         };
-
-//         let mut world = World::new();
-
-//         /*
-
-//         world.spawn((
-//             Group(0),
-//             CharacterTag,
-//             Position {
-//                 x: Fix16::from_whole(32),
-//                 y: Fix16::from_whole(192),
-//             },
-//             Velocity { x: 0, y: 0 },
-//             Direction { x: true, y: true },
-//             CollisionSides {
-//                 top: false,
-//                 right: false,
-//                 bottom: false,
-//                 left: false,
-//             },
-//             Size {
-//                 width: Fix16::from_whole(16),
-//                 height: Fix16::from_whole(32),
-//             },
-//             Health {
-//                 health: 100,
-//                 health_cap: 100,
-//             },
-//             Invulnerability {
-//                 invul_until_frame: 0,
-//                 base_invul_frame: 60,
-//             },
-//             Energy {
-//                 base_energy_cap: 100,
-//                 energy: 100,
-//             },
-//             PowerOutput {
-//                 base_jump_force: -800,
-//                 base_move_speed: 250,
-//             },
-//             Behaviors {
-//                 locked_action: None,
-//                 behaviors: vec![
-//                     Behavior {
-//                         condition: 4,
-//                         action: 5,
-//                         energy_cost: 0,
-//                     },
-//                     Behavior {
-//                         condition: 2,
-//                         action: 3,
-//                         energy_cost: 6,
-//                     },
-//                     Behavior {
-//                         condition: 8,
-//                         action: 2,
-//                         energy_cost: 3,
-//                     },
-//                     Behavior {
-//                         condition: 7,
-//                         action: 8,
-//                         energy_cost: 0,
-//                     },
-//                     Behavior {
-//                         condition: 8,
-//                         action: 7,
-//                         energy_cost: 0,
-//                     },
-//                     Behavior {
-//                         condition: 1,
-//                         action: 1,
-//                         energy_cost: 1,
-//                     },
-//                     Behavior {
-//                         condition: 10,
-//                         action: 0,
-//                         energy_cost: 0,
-//                     },
-//                 ],
-//             },
-//             Weapons {
-//                 selected_weapon_index: 0,
-//                 weapons: vec![Weapon {
-//                     weapon_type: 1,
-//                     projectile_id: Some(1),
-//                     projectile_ejection_method: Some(1),
-//                     energy_cost: 2,
-//                     reload_energy_cost: Some(5),
-//                     reload_cooldown: Some(120),
-//                     ammo_cap: Some(5),
-//                     ammo_count: Some(0),
-//                     rate_of_fire: Some(15),
-//                     output_pos_x: Some(0),
-//                     output_pos_y: Some(12),
-//                     output_count: Some(1),
-//                     recoil: Some((0) * -1),
-//                     require_grounded: false,
-//                     counter: 0,
-//                     reload_start_frame: None,
-//                     last_attack_frame: None,
-//                 }],
-//             },
-//             Protection::from_array([0, 0, 0, 0, 0, 0, 0, 0]),
-//         ));
-
-//         let prng = LCG::new(12345);
-//          */
-//         Ok(())
-//     }
-// }
-
-// #[derive(Accounts)]
-// pub struct Initialize {}
-
 use anchor_lang::prelude::*;
 
 declare_id!("YUe8UbyQ88GmfjUpDE6TwoUbqwaj6qteWqd2VLWoVFm");
@@ -176,14 +11,110 @@ pub mod robot_masters {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Initializing robot-masters program");
 
-        let mut game = Game::init(123, vec![], (9, 10), vec![], vec![], vec![], vec![], vec![]);
+        let mut game = Game::init(
+            123,
+            vec![
+                vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                vec![1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                vec![1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                vec![1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+                vec![1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+                vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+                vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+                vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+                vec![1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+                vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            ],
+            (9, 10),
+            vec![
+                // characters
+                vec![
+                    0,   // id
+                    0,   // group,
+                    16,  // pos x
+                    192, // pos y
+                    16,  // width
+                    32,  // height
+                    1,   // dir, face right
+                    100, // health
+                    100, // energy / energy cap
+                    1,   // energy regen
+                    1,   // energy rate
+                    1,   // power
+                    1,   // weight
+                    100, // punct
+                    100, // blast
+                    100, // force
+                    100, // sever
+                    100, // heat
+                    100, // cryo
+                    100, // jolt
+                    100, // virus
+                ],
+            ],
+            vec![
+                // behaviors
+                vec![
+                    (0, 0), // always condition : shoot action
+                ],
+            ],
+            vec![
+                // spawn definitions
+                vec![
+                    // projectile 1
+                    1,   // health
+                    120, // duration
+                    8,   // damage
+                    1,   // element (punct)
+                    1,   // destroy on collision
+                    0,   // invul
+                    6,   // width
+                    2,   // height
+                    8,   // output x
+                    14,  // output y
+                    0, 0, 0, 0, // extra args (4 in size)
+                    0, 0, 0, // script (rest of the vec)
+                ],
+            ],
+            vec![
+                // action definitions
+                vec![
+                    // shoot projectile 1
+                    2,  // energy cost
+                    30, // interval (rate of fire)
+                    0,  // duration (frames that keeps this action locked)
+                    5,  // limit (ammo cap)
+                    10, // refresh cost (reload energy cost)
+                    60, // refresh duration (reload duration)
+                    0, 0, 0, 0, // extra args (4 in size)
+                    0, 0, 0, // script (rest of the vec, includes reference to projectile 1)
+                ],
+            ],
+            vec![
+                // condition definitions
+                vec![
+                    // always
+                    1, // energy cost multiplier
+                    0, 0, 0, 0, // extra args (4 in size)
+                    0, 0, 0, // script (rest of the vec)
+                ],
+            ],
+        );
 
         msg!("Frame: {:?}", game.frame);
         game.next_frame();
 
         let state = game.export_state();
+        let bytes = state.unwrap();
 
-        let new_game = Game::import_state(&state.unwrap()).unwrap();
+        msg!("Game size: {:?}", bytes.len());
+
+        let new_game = Game::import_state(&bytes).unwrap();
 
         msg!("New Frame: {:?}", new_game.frame);
 
