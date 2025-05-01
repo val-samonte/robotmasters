be618707ccd42bda4320ab37833856e379293ca4
Val Allen Samonte <val.samonte@gmail.com>
2025-05-01 09:18:45 +0800

    Import from anchor

--- robotmasters/programs-ecs/components/game_state/src/lib.rs
+++ robotmasters/programs-ecs/components/game_state/src/lib.rs
@@ -1,13 +1,44 @@
 use bolt_lang::*;
+use borsh::{BorshDeserialize, BorshSerialize};
+extern crate alloc;
 
 declare_id!("5Bdg5zBdxRBSjYxnUkFshi2neQMmiSLHKYAWgkv6VNQQ");
+
+#[derive(Clone)]
+pub struct GameData(pub Box<[u8; 3600]>);
+
+impl Space for GameData {
+    const INIT_SPACE: usize = 3600;
+}
+
+impl BorshSerialize for GameData {
+    fn serialize<W: borsh::maybestd::io::Write>(
+        &self,
+        writer: &mut W,
+    ) -> borsh::maybestd::io::Result<()> {
+        self.0[..].serialize(writer)
+    }
+}
+
+impl BorshDeserialize for GameData {
+    fn deserialize_reader<R: borsh::maybestd::io::Read>(
+        reader: &mut R,
+    ) -> borsh::maybestd::io::Result<Self> {
+        let bytes: [u8; 3600] = BorshDeserialize::deserialize_reader(reader)?;
+        Ok(GameData(Box::new(bytes)))
+    }
+}
+
+impl Default for GameData {
+    fn default() -> Self {
+        GameData(Box::new([0u8; 3600]))
+    }
+}
 
 #[component]
 #[derive(Default)]
 pub struct GameState {
-    pub x: i64,
-    pub y: i64,
-    pub z: i64,
-    #[max_len(20)]
-    pub description: String,
+    pub len: u16,
+    pub frame: u16,
+    pub data: GameData,
 }
