dd2dd9afab15725f142d793d241472a526e6902e
Val Allen Samonte <val.samonte@gmail.com>
2025-05-01 09:31:36 +0800

    Create game and game state

--- robotmasters/programs-ecs/components/game_state/src/lib.rs
+++ robotmasters/programs-ecs/components/game_state/src/lib.rs
@@ -1,32 +1,13 @@
 use bolt_lang::*;
-use borsh::{BorshDeserialize, BorshSerialize};
 extern crate alloc;
 
 declare_id!("5Bdg5zBdxRBSjYxnUkFshi2neQMmiSLHKYAWgkv6VNQQ");
 
-#[derive(Clone)]
+#[derive(Clone, AnchorDeserialize, AnchorSerialize)]
 pub struct GameData(pub Box<[u8; 3600]>);
 
 impl Space for GameData {
     const INIT_SPACE: usize = 3600;
-}
-
-impl BorshSerialize for GameData {
-    fn serialize<W: borsh::maybestd::io::Write>(
-        &self,
-        writer: &mut W,
-    ) -> borsh::maybestd::io::Result<()> {
-        self.0[..].serialize(writer)
-    }
-}
-
-impl BorshDeserialize for GameData {
-    fn deserialize_reader<R: borsh::maybestd::io::Read>(
-        reader: &mut R,
-    ) -> borsh::maybestd::io::Result<Self> {
-        let bytes: [u8; 3600] = BorshDeserialize::deserialize_reader(reader)?;
-        Ok(GameData(Box::new(bytes)))
-    }
 }
 
 impl Default for GameData {
