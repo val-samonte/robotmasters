72758e3622bd35756951c2319e419355135534c9
Val Allen Samonte <val.samonte@gmail.com>
2025-05-01 10:42:05 +0800

    Attempt to run create game system (fails at Return data too large)

--- robotmasters/tests/robotmasters.ts
+++ robotmasters/tests/robotmasters.ts
@@ -1,86 +1,74 @@
-import { PublicKey } from "@solana/web3.js";
-import { Position } from "../target/types/position";
-import { Movement } from "../target/types/movement";
+import { PublicKey } from '@solana/web3.js'
 import {
-    InitializeNewWorld,
-    AddEntity,
-    InitializeComponent,
-    ApplySystem,
-    Program
-} from "@magicblock-labs/bolt-sdk"
-import {expect} from "chai";
-import * as anchor from "@coral-xyz/anchor";
+  InitializeNewWorld,
+  AddEntity,
+  InitializeComponent,
+  ApplySystem,
+  Program,
+} from '@magicblock-labs/bolt-sdk'
+import { expect } from 'chai'
+import * as anchor from '@coral-xyz/anchor'
+import { CreateGame } from '../target/types/create_game'
+import { GameState } from '../target/types/game_state'
 
-describe("robotmasters", () => {
+describe('robotmasters', () => {
   // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env();
-  anchor.setProvider(provider);
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
 
   // Constants used to test the program.
-  let worldPda: PublicKey;
-  let entityPda: PublicKey;
-  let componentPda: PublicKey;
+  let worldPda: PublicKey
+  let entityPda: PublicKey
+  let componentPda: PublicKey
 
-  const positionComponent = anchor.workspace.Position as Program<Position>;
-  const systemMovement = anchor.workspace.Movement as Program<Movement>;
+  const gameStateComponent = anchor.workspace.GameState as Program<GameState>
+  const systemCreateGame = anchor.workspace.CreateGame as Program<CreateGame>
 
-  it("InitializeNewWorld", async () => {
+  it('InitializeNewWorld', async () => {
     const initNewWorld = await InitializeNewWorld({
       payer: provider.wallet.publicKey,
       connection: provider.connection,
-    });
-    const txSign = await provider.sendAndConfirm(initNewWorld.transaction);
-    worldPda = initNewWorld.worldPda;
-    console.log(`Initialized a new world (ID=${worldPda}). Initialization signature: ${txSign}`);
-  });
+    })
+    const txSign = await provider.sendAndConfirm(initNewWorld.transaction)
+    worldPda = initNewWorld.worldPda
+    console.log(
+      `Initialized a new world (ID=${worldPda}). Initialization signature: ${txSign}`
+    )
+  })
 
-  it("Add an entity", async () => {
+  it('Add an entity', async () => {
     const addEntity = await AddEntity({
       payer: provider.wallet.publicKey,
       world: worldPda,
       connection: provider.connection,
-    });
-    const txSign = await provider.sendAndConfirm(addEntity.transaction);
-    entityPda = addEntity.entityPda;
-    console.log(`Initialized a new Entity (ID=${addEntity.entityId}). Initialization signature: ${txSign}`);
-  });
+    })
+    const txSign = await provider.sendAndConfirm(addEntity.transaction)
+    entityPda = addEntity.entityPda
+  })
 
-  it("Add a component", async () => {
+  it('Add a component', async () => {
     const initializeComponent = await InitializeComponent({
       payer: provider.wallet.publicKey,
       entity: entityPda,
-      componentId: positionComponent.programId,
-    });
-    const txSign = await provider.sendAndConfirm(initializeComponent.transaction);
-    componentPda = initializeComponent.componentPda;
-    console.log(`Initialized the grid component. Initialization signature: ${txSign}`);
-  });
+      componentId: gameStateComponent.programId,
+    })
+    await provider.sendAndConfirm(initializeComponent.transaction)
+    componentPda = initializeComponent.componentPda
+  })
 
-  it("Apply a system", async () => {
-    // Check that the component has been initialized and x is 0
-    const positionBefore = await positionComponent.account.position.fetch(
-      componentPda
-    );
-    expect(positionBefore.x.toNumber()).to.equal(0);
-
+  it('Apply a system', async () => {
     // Run the movement system
     const applySystem = await ApplySystem({
       authority: provider.wallet.publicKey,
-      systemId: systemMovement.programId,
+      systemId: systemCreateGame.programId,
       world: worldPda,
-      entities: [{
-        entity: entityPda,
-        components: [{ componentId: positionComponent.programId }],
-      }]
-    });
-    const txSign = await provider.sendAndConfirm(applySystem.transaction);
-    console.log(`Applied a system. Signature: ${txSign}`);
-
-    // Check that the system has been applied and x is > 0
-    const positionAfter = await positionComponent.account.position.fetch(
-      componentPda
-    );
-    expect(positionAfter.x.toNumber()).to.gt(0);
-  });
-
-});
+      entities: [
+        {
+          entity: entityPda,
+          components: [{ componentId: gameStateComponent.programId }],
+        },
+      ],
+    })
+    await provider.sendAndConfirm(applySystem.transaction)
+  })
+})
